578382e944ef254c698ad40ee09e7a3598753246
kapil-infobeans <kapil.yadav@infobeans.com>
2017-11-08 19:59:11 +0530

    Math Plugin complete unittest with dataproviders

--- unittest/testMath.php
+++ unittest/testMath.php
@@ -31,12 +31,12 @@ class testMath extends PHPUnit_Framework_TestCase {
      */
     public function testMathAdd($number1, $number2, $expected) {
         $this->assertEquals(
-                $expected, $this->object->mathAdd($number1, $number2)
+                $expected, $this->object->mathAdd($number1, $number2), "Adding " . $number1 . " and " . $number2 . " did not return " . $expected
         );
     }
 
     /**
-     * MathAdd Data Provider
+     * mathAdd Data Provider
      * 
      * @since   1.0
      */
@@ -50,6 +50,99 @@ class testMath extends PHPUnit_Framework_TestCase {
             ),
             array(
                 4, -3, 'expected' => 1
+            )
+        );
+    }
+
+    /**
+     * @covers Math::mathSubstration
+     * @dataProvider mathSubstrationDataProvider
+     * @return void
+     * Substraction Method
+     */
+    public function testMathSubstration($number1, $number2, $expected) {
+        $this->assertEquals(
+                $expected, $this->object->mathSubstration($number1, $number2), "Subsctracting " . $number1 . " and " . $number2 . " did not return " . $expected
+        );
+    }
+
+    /**
+     * mathSubstration Data Provider
+     * 
+     * @since   1.0
+     */
+    public function mathSubstrationDataProvider() {
+        return array(
+            array(
+                2, 3, 'expected' => -1
+            ),
+            array(
+                7, 5, 'expected' => 2
+            ),
+            array(
+                4, -3, 'expected' => 7
+            )
+        );
+    }
+
+    /**
+     * @covers Math::mathMultiply
+     * @dataProvider mathMultiplyDataProvider
+     * @return void
+     * Multiplication Method
+     */
+    public function testMathMultiply($number1, $number2, $expected) {
+        $this->assertEquals(
+                $expected, $this->object->mathMultiply($number1, $number2), "Multiplying " . $number1 . " and " . $number2 . " did not return " . $expected
+        );
+    }
+
+    /**
+     * mathMultiply Data Provider
+     * 
+     * @since   1.0
+     */
+    public function mathMultiplyDataProvider() {
+        return array(
+            array(
+                2, 3, 'expected' => 6
+            ),
+            array(
+                7, -5, 'expected' => -35
+            ),
+            array(
+                0, 1, 'expected' => 0
+            )
+        );
+    }
+    
+    /**
+     * @covers Math::mathDivision
+     * @dataProvider mathDivisionDataProvider
+     * @return void
+     * Divison Method
+     */
+    public function testMathDivision($number1, $number2, $expected) {
+        $this->assertEquals(
+                $expected, $this->object->mathDivision($number1, $number2), "Divide " . $number1 . " and " . $number2 . " did not return " . $expected
+        );
+    }
+
+    /**
+     * mathDivision Data Provider
+     * 
+     * @since   1.0
+     */
+    public function mathDivisionDataProvider() {
+        return array(
+            array(
+                4, 2, 'expected' => 2
+            ),
+            array(
+                1, 0, 'expected' => false
+            ),
+            array(
+                0, 1, 'expected' => 0
             )
         );
     }
