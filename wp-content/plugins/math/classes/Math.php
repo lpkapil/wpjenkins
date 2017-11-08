2eb76b016ff5264ce9fdcbe2cf614c6f938ea23d
kapil-infobeans <kapil.yadav@infobeans.com>
2017-11-08 20:34:21 +0530

    Fixed phpcs errors

--- wp-content/plugins/math/classes/Math.php
+++ wp-content/plugins/math/classes/Math.php
@@ -1,74 +1,106 @@
 <?php
 
 /**
- * Math API
+ * Main Math Class
  *
- * @author kapil
+ * @category Class
+ * @package  Math
+ * @author   Kapil Yadav <lpkapil@gmail.com>
+ * @license  open source
+ * @version  GIT: https://github.com/lpkapil/wpjenkins
+ * @link     https://github.com/lpkapil/wpjenkins
  */
-class Math {
+
+/**
+ * Math Class Doc Comment
+ *
+ * @category Class
+ * @package  Math
+ * @author   Kapil Yadav <lpkapil@gmail.com>
+ * @license  open source
+ * @version  Release: prod_1.0
+ * @link     https://github.com/lpkapil/wpjenkins
+ *
+ */
+class Math
+{
+
+    /**
+     * Class member variables
+     * 
+     * @since   1.0
+     */
+    private $_version, $_prefix;
 
     /**
      * Class constructor
      * 
-     * @since   1.0
+     * @since 1.0
      */
-    private $version, $prefix;
+    public function __construct()
+    {
 
-    public function __construct() {
-
-        $this->version = '1.0';
-        $this->prefix = 'math';
+        $this->_version = '1.0';
+        $this->_prefix = 'math';
     }
 
     /**
      * Get Addition 
      * 
+     * @param integer $a parameter 1 
+     * @param integer $b parameter 2
+     * 
      * @Method mathAdd
-     * 
      * @return INT32
-     * 
      * @since   1.0
      */
-    public function mathAdd($a = 0, $b = 0) {
+    public function mathAdd($a = 0, $b = 0)
+    {
         return $a + $b;
     }
 
     /**
      * Get Substraction 
      * 
+     * @param integer $a parameter 1 
+     * @param integer $b parameter 2
+     * 
      * @Method mathSubstration
-     * 
      * @return INT32
-     * 
      * @since   1.0
      */
-    public function mathSubstration($a = 0, $b = 0) {
+    public function mathSubstration($a = 0, $b = 0)
+    {
         return $a - $b;
     }
 
     /**
      * Get Multiplication
      * 
+     * @param integer $a parameter 1 
+     * @param integer $b parameter 2
+     * 
      * @Method mathMultiply
-     * 
      * @return INT32
-     * 
      * @since   1.0
      */
-    public function mathMultiply($a = 0, $b = 0) {
+    public function mathMultiply($a = 0, $b = 0)
+    {
         return $a * $b;
     }
 
     /**
      * Get Devision
      * 
+     * @param integer $a parameter 1 
+     * @param integer $b parameter 2
+     * 
      * @Method mathDivision
-     * 
      * @return INT32
-     * 
      * @since   1.0
      */
-    public function mathDivision($a = 0, $b = 0) {
+    public function mathDivision($a = 0, $b = 0)
+    {
 
         //Devide by zero
         if ($b == 0) {
--- wp-content/plugins/math/index.php
+++ wp-content/plugins/math/index.php
@@ -1,23 +1,29 @@
 <?php
 
 /**
- * @package Math
+ * Math Plugin Main File
+ * 
+ * @category Plugin
+ * @package  Math
+ * @author   Kapil Yadav <lpkapil@gmail.com>
+ * @license  open source
+ * @version  GIT: https://github.com/lpkapil/wpjenkins
+ * @link     https://github.com/lpkapil/wpjenkins
  */
-/*
-  Plugin Name: Math
-  Description: This is Mathematics Class.
-  Version: 1.0
-  Author: Kapil Yadav
-  Text Domain: math
+/**
+ * Plugin Name: Math
+ * Description: This is Mathematics Class.
+ * Version: 1.0
+ * Author: Kapil Yadav
+ * Text Domain: math
  */
-
 if (!defined('ABSPATH')) {
     header('Status: 403 Forbidden');
     header('HTTP/1.1 403 Forbidden');
     exit;
 }
 
-include 'classes/Math.php';
+require 'classes/Math.php';
 
 new Math();
 
