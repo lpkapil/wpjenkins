4a81380f900376b071e98806685654477125b961
cr@zy3xpl0!t <lpkapil@gmail.com>
2020-04-25 20:29:02 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wpjenkins');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wpjenkins');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'wpjenkins');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
